f5c71d1b069e21f308045b07e264998d80a92d3d
nour-alsatari <nouress98@gmail.com>
2022-01-23 16:08:25 +0200

    js2 task

--- Js/app.js
+++ Js/app.js
@@ -24,3 +24,37 @@ if (welcomeMessagePrefrence)
     alert (`Welcome to the website ${title}${userName}`)
 }
 console.log(title);
+
+// today's task
+
+let answer1 = prompt ("Do you enjoy coding?", "yes or no");
+let answer2 = prompt ("Do you spesk english?", "yes or no");
+let answer3 = prompt ("Do you like food?", "yes or no");
+let answersArray = [];
+
+function validateAnswers(){
+if (answer1 != "yes" && answer1 != "no"){
+    answer1 = "invalid";
+}
+console.log(answer1);
+
+if (answer2 != "yes" && answer2 != "no"){
+    answer2 = "invalid";
+}
+console.log(answer2);
+
+if (answer3 != "yes" && answer3 != "no"){
+    answer3 = "invalid";
+}
+console.log(answer3);
+}
+
+validateAnswers();
+
+answersArray.push(answer1);
+answersArray.push(answer2);
+answersArray.push(answer3);
+
+for (let i = 0 ; i < answersArray.length ; i++){
+console.log(answersArray[i])
+} 
